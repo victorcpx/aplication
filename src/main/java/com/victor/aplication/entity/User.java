1a44cd3c7cf56cd94b7aa061482c468e5c2f1912
Victor <victor-1999@hotmail.es>
2024-11-19 23:46:27 +0100

    #6 Update User terminado

--- src/main/java/com/victor/aplication/entity/User.java
+++ src/main/java/com/victor/aplication/entity/User.java
@@ -66,6 +66,10 @@ public class User implements Serializable {
 	public Long getId() {
 		return id;
 	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 	
     public Set<Role> getRoles() {
         return roles;
